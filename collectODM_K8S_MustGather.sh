d81c2a217c1905608495934378a53be1688c07e2
Russell Scher <42454585+rscher@users.noreply.github.com>
2022-09-15 11:17:31 -0700

    version 1 release for MustGather

--- collectODM_K8S_MustGather.sh
+++ collectODM_K8S_MustGather.sh
@@ -3,30 +3,28 @@
 #
 #  collectODM_K8S_MustGather.sh
 #
-#  -ODM on CP4BA/OpenShift/Kubernetes, MustGather script- 
+#  -ODM on CP4BA/OpenShift/Kubernetes, MustGather script-
 #   Collects diagnostics, system info and logs for a specified namespace
-#   
+#
 #  Usage: collectODM_K8S_MustGather.sh  arg1=<namespace>  arg2=[deployment]  [options]
-#   run 'collectODM_K8S_MustGather.sh' with no args for help
-#   run 'collectODM_K8S_MustGather.sh options' for command options
-#    
+#   Use "collectODM_K8S_MustGather.sh " for command options
+#
 #   Required:
-#    arg1 =  <namespace>
+#    arg1=<namespace>
 #
 #   Optional:
-#    deployment specifier 
-#    arg2 = deployment={DC, DSR, DR, DSC}  
-#      values must be one of {DC, DSR, DR or DSC}  
-#      arg2 default value is blank (collects logs for all running ODM pods in namespace)
-#      arg2 limits collecting MustGather to the specified deployment
+#    arg2=[ deployment={DC, DSR, DR, DSC} ] 
+#      limits collecting MustGather for the specified deployment
+#      value must be one of {DC, DSR, DR or DSC} 
+#      default value is blank (collects logs for all running ODM pods in namespace)
 #
-#    example usage:  
-#     collect logs from all ODM pods running in namespace 'cp2201' 
-#      $ collectODM_K8S_MustGather.sh cp2201 
-# 
-#     collect logs for decisionserverruntime pods running in namespace  specified by var $cp_ns 
-#     if replicaSet has 10 dsr pods running, will collect 10 dsr log files 
-#       $ collectODM_K8S_MustGather.sh $cp_ns DSR 
+#    example usage:
+#     collect logs for all ODM pods running in namespace 'cp2201'
+#      $ collectODM_K8S_MustGather.sh cp2201
+#
+#     collect logs for decisionserverruntime (DSR) deployment running in namespace 'cp2201'
+#     if replicaSet has 10 dsr pods running, script will collect MustGather for 10 dsr pods 
+#       $ collectODM_K8S_MustGather.sh cp2201 DSR
 #
 #    Output archive: ./MustGatherODM_K8S.<namespace>.<timestamp>.tar
 #
@@ -34,43 +32,44 @@
 #      ./system-info.log (Host/OS info)
 #      ./container_images.log  (lists container images with digests from registry)
 #      ./kube_config.log (Kubernetes config info , tokens/secrets are redacted)
-#      ./<namespace_podname>.log created for each running pod in namespace      
+#      ./<namespace_podname>.log created for each running pod in namespace
 #
 # -------------------------
 #
 #    Command Options:
-#     --help (show usage)
-#     --javadump  (collect javadumps and heapdumps)
-#     --remove_javadumps (remove javadumps from specified containers - Maintenance task only)
-#     --clusterdump (Warning: requires cluster admin role, task is CPU intensive)
+#     --help 
+#       (show usage)
+#     --javadump  
+#       (generate Liberty javacore and heapdump inside the container in addition to pod logs. Does not remove the generated dump from container)
+#     --remove_javadumps 
+#       (remove all generated javadumps from specified containers - Maintenance task only, does not collect logs or javadumps))
+#     --clusterdump
+#       (runs 'kubectl cluster-info dump'. Requires cluster admin role, option is CPU intensive and intended for limited usage) 
 #
-#    example usage of --javadump option:
-#    Generate and include javadumps (heap and thread dumps) from each DSR pod in $cp_ns namespace
-#     $ collectODM_K8S_MustGather.sh $cp_ns DSR --javadump 
-#  
-#    example usage of --remove_javadumps option:
-#     Removes all generated javadumps from each pod in $cp_ns namespace 
-#     $ collectODM_K8S_MustGather.sh $cp_ns  --remove_javadumps
+#     example usage of --javadump option:
+#      Collect logs and include javadumps (heap and thread dumps) from each DSR pod in 'cp2201' namespace
+#      $ collectODM_K8S_MustGather.sh cp2201 DSR --javadump
 #
-#     Removes all generated javadumps from DSR pods in $cp_ns namespace 
-#     $ collectODM_K8S_MustGather.sh $cp_ns DSR  --remove_javadumps
+#     example usage of --remove_javadumps option:
+#      Removes all generated javadumps from each pod in 'cp2201' namespace
+#      $ collectODM_K8S_MustGather.sh cp2201 --remove_javadumps
 #
+#   Generated tar file contents:
 #     Logs/javadumps: archive created for each running pod in namespace
 #     ./<namespace>.<podname>.tar
-#     ./opt/ibm/wlp/output/defaultServer/
+#     ./opt/ibm/wlp/output/defaultServer/heapdump.<timestamp>.phd
+#     ./opt/ibm/wlp/output/defaultServer/javacore.<timestamp>.txt
 #
-#      heapdump.<timestamp>.phd
-#      javacore.<timestamp>.txt
+#  Note: If there are no running ODM containers, 
+#        script will collect System/Cluster/Image Registry Info only
 #
-#     Note: If there are no running ODM containers, script will generate System/CLuster/Image Registry Info only
-#
-##########################################################
+######################################################################
 
 unset dbserver
-unset dcPods 
+unset dcPods
 unset dsrPods
-unset drPods 
-unset dscPods 
+unset drPods
+unset dscPods
 unset deployment
 
 unset cli_cmd
@@ -84,9 +83,13 @@ unset clusterdump
 unset exitMode
 
 CUR_DIR=$(pwd)
-TEMP_FOLDER="/home/$USER/.tmp"
+TEMP_FOLDER="$HOME/.tmp"
 TIMESTAMP=`date "+%Y%m%d.%H%M%S"`
+red='\e[31m' ; clear='\e[0m'
 DEV_NULL=/dev/null
+
+#---cmds----
+validate_ns="kubectl get ns $1 -o name  --ignore-not-found=true | grep $1 "
 wlpServerPathContainer=/opt/ibm/wlp/output/defaultServer
 wlpServerCmdContainer=/liberty/wlp/bin/server
 
@@ -97,16 +100,15 @@ drPods="decisionrunner"
 dscPods="decisionserverconsole"
 
 # ----- functions -------
-
 function getPodLogs()
 {
- pods=$(kubectl get pods --field-selector=status.phase=Running | grep $deployment | gawk  '{print $1}')
+ pods=$(kubectl get pods --field-selector=status.phase=Running | grep $deployment | gawk '{print $1}')
  for pod in $pods
  do
   if [[ $remove_javadumps == "true" ]] ; then
    echo "removing all existing javadumps from pod/$pod in deployment/$deployment ..."
-   heapdumps=$(kubectl exec -n $ns $pod -- ls -l $wlpServerPathContainer/ | grep heapdump | gawk '{ print $9 }' )
-   javacores=$(kubectl exec -n $ns $pod -- ls -l $wlpServerPathContainer/ | grep javacore | gawk '{ print $9 }' )
+   heapdumps=$(kubectl exec -n $ns $pod -- ls -l $wlpServerPathContainer/ | grep heapdump | gawk '{ print $9 }')
+   javacores=$(kubectl exec -n $ns $pod -- ls -l $wlpServerPathContainer/ | grep javacore | gawk '{ print $9 }')
    files=$(echo $javacores $heapdumps)
    for file in $files
    do
@@ -114,12 +116,12 @@ function getPodLogs()
    done
   elif [[ $javadumps == "true" ]] ; then
    echo "Generating javadumps for pod/$pod in deployment/$deployment ..."
-   kubectl exec -n $ns $pod -- $wlpServerCmdContainer javadump defaultServer --include=heap,thread >> $TEMP_FOLDER/javadump.$TIMESTAMP.log   
-   kubectl exec -n $ns $pod -- tar cf - $wlpServerPathContainer > $TEMP_FOLDER/$pod.tar
+   kubectl exec -n $ns $pod -- $wlpServerCmdContainer javadump defaultServer --include=heap,thread >> $TEMP_FOLDER/javadump.$TIMESTAMP.log
+   kubectl exec -n $ns $pod -- tar cf  - $wlpServerPathContainer > $TEMP_FOLDER/$pod.tar
    echo "Collecting logs for pod/$pod in deployment/$deployment ..."
    kubectl describe pod $pod  &> ${TEMP_FOLDER}/describe-$pod.log
    kubectl logs $pod  &> ${TEMP_FOLDER}/$pod.log
-  else 
+  else
    echo "Collecting logs for pod/$pod in deployment/$deployment  ..."
    kubectl describe pod $pod  &> ${TEMP_FOLDER}/describe-$pod.log
    kubectl logs $pod  &> ${TEMP_FOLDER}/$pod.log
@@ -136,12 +138,12 @@ if [[ ! "$deployment" ]] ; then
    getPodLogs $deployment
   done
 else
-  getPodLogs $deployment 
+  getPodLogs $deployment
 fi
 }
 
 
-function separator 
+function separator
 {
  echo "_________________________________________"  &>> ${TEMP_FOLDER}/system-info.log
  echo ""  &>> ${TEMP_FOLDER}/system-info.log
@@ -167,7 +169,7 @@ kubectl version  &>> ${TEMP_FOLDER}/system-info.log
 if [[ $clusterdump == "true" ]] ; then
   echo "running 'kubectl cluster-info dump' ..."
   kubectl cluster-info dump &> ${TEMP_FOLDER}/kube_cluster_dump.log
-  echo "kubectl cluster-info dump output saved to kube_cluster_dump.log in MustGather tar file" 
+  echo "kubectl cluster-info dump output saved to kube_cluster_dump.log in MustGather tar file"
 fi
 kubectl config view &>  ${TEMP_FOLDER}/kube_config.log
 # check for OpenShift
@@ -189,8 +191,8 @@ then
 fi
  echo "Container runtime info:" &>> ${TEMP_FOLDER}/system-info.log
 ${cli_cmd} version  &>> ${TEMP_FOLDER}/system-info.log
-${cli_cmd} info  &>> ${TEMP_FOLDER}/system-info.log 
-${cli_cmd} images --digests &> ${TEMP_FOLDER}/container_images.log 
+${cli_cmd} info  &>> ${TEMP_FOLDER}/system-info.log
+${cli_cmd} images --digests &> ${TEMP_FOLDER}/container_images.log
 
 if command -v "skopeo" &> $DEV_NULL
 then
@@ -199,7 +201,7 @@ fi
 separator
 
 echo "Helm info:" &>> ${TEMP_FOLDER}/system-info.log
-helm version  &>> ${TEMP_FOLDER}/system-info.log 
+helm version  &>> ${TEMP_FOLDER}/system-info.log
 echo "Helm charts deployed:" &>> ${TEMP_FOLDER}/system-info.log
 helm ls -A &>> ${TEMP_FOLDER}/system-info.log
 separator
@@ -208,79 +210,74 @@ separator
 
 function usage_options()
 {
- echo  " Command Options:"
- echo  "  --help      (print help/usage)"
- echo  "  --javadump  (include option to collect javadumps and heapdumps )"
- echo  "  --remove_javadumps (remove javadumps from specified containers)"
- echo  "      (Maintenance task only, does not collect new logs or javadumps)"
- echo  "  --clusterdump (Warning: requires cluster admin role, task is CPU intensive)"
+ echo " Command Options:"
+ echo "  --help      (print help/usage)"
+ echo "  --javadump"  
+ echo "     (include option to collect javadumps and heapdumps in addition to pod logs. Does not remove the generated dump from containers.)"
+ echo "  --remove_javadumps"
+ echo "     (remove javadumps from specified containers - Maintenance task only, does not collect new logs or javadumps)"
+ echo "  --clusterdump"
+ echo "     (runs 'kubectl cluster-info dump'. Requires cluster admin role, option is CPU intensive and intended for limited usage)"
  echo ""
- echo "  examples:"
+ echo " Examples:"
  echo "  Collect logs and javadumps from all pods running in odm8105 namespace"
- echo " $ collectODM_K8S_MustGather.sh odm8105 --javadump"
+ echo "  $ collectODM_K8S_MustGather.sh odm8105 --javadump"
  echo ""
  echo "  Collect logs and javadumps from all decisionserverruntime pods running in cp2103 namespace "
- echo " $ collectODM_K8S_MustGather.sh  cp2103 DSR --javadump"
+ echo "  $ collectODM_K8S_MustGather.sh  cp2103 DSR --javadump"
  echo ""
- echo " Remove all generated javadumps from each pod in odm8110 namespace"
- echo " $ collectODM_K8S_MustGather.sh  odm8110 --remove_javadumps"
- echo ""
- echo " Remove all generated javadumps from decisionserverruntime pods in cp2201 namespace"
- echo "  $ collectODM_K8S_MustGather.sh  cp2201 DSR --remove_javadumps"
+ echo "  Remove all generated javadumps from each pod in odm8110 namespace"
+ echo "  $ collectODM_K8S_MustGather.sh  odm8110 --remove_javadumps"
  echo ""
 }
 
 function usage()
 {
-echo " usage: collectODM_K8S_MustGather.sh  arg1=<namespace> [arg2=<deployment>] [options]"
+echo " usage: collectODM_K8S_MustGather.sh  arg1=<namespace> [arg2=<deployment>] [options]" 
 echo "   use 'collectODM_K8S_MustGather.sh  options' for command options"
 echo ""
  if [[ $javadumps == "true" ]]  ||  [[ $remove_javadumps == "true" ]] ; then
  usage_options
 else
  echo " Required:"
- echo "  arg1 = <namespace>" 
- echo ""
- echo "  example:"
- echo "   Collect all logs from all ODM pods running in namespace odm81101 "
- echo "  $ collectODM_K8S_MustGather.sh  odm81101"
+ echo "  arg1=<namespace>"
  echo ""
  echo " [Optional]:"
- echo "  arg2 = deployment={DC, DSR, DR, DSC}"
- echo "    default value is blank (collects MustGather logs for all running ODM pods in namespace)"
- echo "  arg2 limits collecting MustGather to the specified deployment"
+ echo "  arg2=[ deployment={DC, DSR, DR, DSC} ] "
+ echo "    limits collecting MustGather for the specified deployment"
+ echo "    value must be one of {DC, DSR, DR or DSC}"
+ echo "    default value is blank (collects logs for all running ODM pods in namespace)"
  echo ""
- echo "  example:"
+ echo "  examples:"
+ echo "   Collect all logs from all ODM pods running in namespace odm81101 "
+ echo "   $ collectODM_K8S_MustGather.sh  odm81101"
  echo ""
- echo "  Collect logs for only decisionserverruntime (DSR) pods running in cp2202 namespace 
-         if replicaSet has 10 DSR pods running, will collect 10 DSR log files"
- echo "  $ collectODM_K8S_MustGather.sh  cp2202 DSR"
+ echo "   Collect logs for only decisionserverruntime (DSR) pods running in cp2202 namespace"
+ echo "   if replicaSet has 10 DSR pods running, will collect 10 DSR log files"
+ echo "   $ collectODM_K8S_MustGather.sh  cp2202 DSR"
  echo ""
 fi
 }
 
-
 # -----------------
-#  --- main --- 
-# ----------------- 
+#  --- main ---
+# -----------------
 
 if [ ! -d  $TEMP_FOLDER ] ; then
  mkdir -p $TEMP_FOLDER  &> $DEV_NULL
 fi
 
 # validate arg1 - required namespace
-if [ -z $1 ] ; then
- exitMode="usage"
-elif [[ $1 == "--help" ]] ; then
+if [ -z $1 ] || [[ $1 == "--help" ]]   ; then
  exitMode="usage"
 elif [[ $1 == "options" ]] ; then
  exitMode="usage"
  javadumps=true
-elif [[ $(kubectl get ns $1 -o json  --ignore-not-found=true  ) == "" ]] ; then
- echo "error: namespace $1 not found in cluster"
+elif [[ $($validate_ns) == "" ]] ; then
+ echo -e $red"error: namespace $1 not found in cluster."$clear ; echo "" 
  exitMode="usage"
 else
- # valid namespace, set as current context
+ # namespace is valid, set as current context
  ns=$1
  kubectl config set-context --current --namespace=$ns &> $DEV_NULL
  unset exitMode
@@ -298,7 +295,7 @@ if [[ $2 != "" ]] ; then
    --remove_javadumps) remove_javadumps=true ;;
    --clusterdump) clusterdump=true ;;
    --help) exitMode="usage" ;;
-   *) echo "error: $2 not a valid option" ; echo "" ; exitMode="usage" ;;
+   *) echo -e $red"error: $2 not a valid option"$clear ; echo "" ; exitMode="usage" ;;
   esac
 fi
 
@@ -313,14 +310,14 @@ elif  [[ $3 == "--clusterdump" ]] ; then
   unset remove_javadumps
   clusterdump=true
 elif [[ $3 == "--help" ]] ; then
- exitMode="usage" 
+ exitMode="usage"
 elif  [[ $3 != "" ]] ; then
  echo "warning: ignoring $3"
  echo ""
 fi
 
 if [[ $exitMode == "usage" ]] ; then
-  usage ;  exit 
+  usage ;  exit
 elif [[ ! $remove_javadumps ]] ; then
  getSystemInfo
 fi
@@ -330,7 +327,7 @@ getODMPodLogs
 if [[ $remove_javadumps ]] ; then
  echo "Completed removing javadumps from all odm containers in namespace $ns"
 else
- tar -cf ./MustGatherODM_K8S.$ns.$TIMESTAMP.tar -C  ${TEMP_FOLDER} .  &> $DEV_NULL    
+ tar -cf ./MustGatherODM_K8S.$ns.$TIMESTAMP.tar -C  ${TEMP_FOLDER} .  &> $DEV_NULL
  echo "Completed collecting MustGather logs. Attach ./MustGatherODM_K8S.$ns.$TIMESTAMP.tar to support case."
 fi
 
